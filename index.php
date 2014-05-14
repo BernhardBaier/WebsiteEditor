f4829cde0b56f145e69ac869f56fd62b19f235e9
Bernhard Baier <bernhard.baier@baier-consulting.de>
2014-05-14 15:16:25 +0200

    fixed mobile detection

--- index.php
+++ index.php
@@ -6,4 +6,52 @@
  * Time: 17:08
  */
 //ToDo: find out browser version and relink to correponding page!
-include('mobile.php');
+$html5 = false;
+$mobile = false;
+if($_GET['forceMobile'] == 'true'){
+    $mobile = true;
+}
+$browser = $_SERVER['HTTP_USER_AGENT'];
+if(strpos($browser,'Firefox') > -1){
+    if(substr($browser,strpos($browser,'Firefox')+8,4) >= 16){
+        $html5 = true;
+        if(strpos($browser,'Mobile') > -1){
+            $mobile = true;
+        }
+    }
+}elseif(strpos($browser,'Chrome') > -1){
+    if(substr($browser,strpos($browser,'Chrome')+7,4) >= 21){
+        $html5 = true;
+        if(strpos($browser,'Mobile') > -1){
+            $mobile = true;
+        }
+    }
+}elseif(strpos($browser,'OPR/') > -1){
+    if(substr($browser,strpos($browser,'OPR/')+4,4) >= 10.5){
+        $html5 = true;
+        if(strpos($browser,'Mobile') > -1){
+            $mobile = true;
+        }
+    }
+}elseif(strpos($browser,'MSIE') > -1){
+    if(substr($browser,strpos($browser,'MSIE')+5,2) >= 10){
+        $html5 = true;
+        if(strpos($browser,'Windows Phone') > -1){
+            $mobile = true;
+        }
+    }
+}elseif(strpos($browser,'rv:') > -1 && strpos($browser,'like Gecko') > -1){
+    if(substr($browser,strpos($browser,'rv:')+3,2) >= 10){
+        $html5 = true;
+        if(strpos($browser,'Windows Phone') > -1){
+            $mobile = true;
+        }
+    }
+}
+if($html5 === true){
+    if($mobile === true){
+        include('mobile.php');
+    }else{
+        include('html5.php');
+    }
+}
