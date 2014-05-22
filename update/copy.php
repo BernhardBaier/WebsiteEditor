a38468b455137dd4f8d28a04e212abee988a464c
Bernhard Baier <bernhard.baier@baier-consulting.de>
2014-05-22 17:27:16 +0200

    updater can create dirs

--- update/copy.php
+++ update/copy.php
@@ -3,6 +3,14 @@ include "auth.php";
 if($authLevel == '1111'){
     $remotePath = $_POST['remotePath'];
     $path = $_POST['path'];
+    $dir = substr($path,0,strrpos($path,'/'));
+    if(!is_dir($dir)){
+        if(!mkdir($dir)){
+            $dir2 = substr($dir2,0,strrpos($dir2,'/'));
+            mkdir($dir2);
+            mkdir($dir);
+        }
+    }
     if(copy($remotePath.substr($path,3),$path)){
         echo('1');
     }else{
